aa5095628e0153691c326b5de87173954e75fa61
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-10-15 15:41:18 +0600

    route of recipe

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -4,27 +4,27 @@ import React from "react";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-red-500">
+    <div className=" text-slate-800">
       {/* WRAPPER */}
-      <div className="w-max flex">
+      <div className=" grid grid-cols-3 gap-3 w-[90%] mx-auto px-3">
         {/* SINGLE ITEM */}
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+            className=" shadow-md rounded-md p-3 hover:bg-fuchsia-50 transition-all duration-300 "
           >
             {/* IMAGE CONTAINER */}
             {item.img && (
-              <div className="relative flex-1 w-full  hover:rotate-[60deg] transition-all duration-500">
-                <Image src={item.img} alt="" className="object-contain" height={100} width={100} />
+              <div className=" hover:rotate-[60deg] transition-all duration-500">
+                <Image src={item.img} alt="" width={200} height={200} className="object-contain"  />
               </div>
             )}
             {/* TEXT CONTAINER */}
-            <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
+            <div className="">
               <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
-              <p className="p-4 2xl:p-8">{item.desc}</p>
+              <p className="my-2">{item.desc}</p>
               <span className="text-xl font-bold">${item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
+              <button  className="ml-3 bg-red-500 text-white p-2 rounded-md">
                 Add to Cart
               </button>
             </div>
---
+++ pages/recipe.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RecipeForm from '../components/RecipeForm'
+
+type Props = {}
+
+const recipe = (props: Props) => {
+  return (
+    <div>
+        <RecipeForm />
+    </div>
+  )
+}
+
+export default recipe
