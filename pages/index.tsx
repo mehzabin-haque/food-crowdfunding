65919ba5d2522d64da3db68c9963e3d8f00be719
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-10-15 09:07:55 +0600

    Tested auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session, status } = useSession()
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
